69f3374472472a17417b368d2e0ffd9efe7ee4b3
guillelopez22 <guillelopez_13@hotmail.com>
2017-06-08 04:48:14 -0600

    POST PUT REMOVE

--- controllers/libroController.js
+++ controllers/libroController.js
@@ -2,39 +2,51 @@ var student = require('../schemas/libro');
 
 
 
-exports.e
-createLibro
-updateLibro
-deleteLibro
-buscartodoL
-todoLibro},
-buscarLibro
-marcarLibro
-exports.getStudents = {
-  auth: {
-    mode:'required',
-    strategy:'session',
-    scope: ['admin', 'regular']
-  },
+exports.createLibro = {
   handler: function(request, reply){
-    var students = student.find({});
-    reply(students);
-  }
-}
-
-exports.createStudent = {
-  auth: {
-    mode:'required',
-    strategy:'session',
-    scope: ['admin']
-  },
-  handler: function(request, reply){
-    var newStudent = new student({
-      name: request.payload.name,
-      account: request.payload.account
+    var newLibro = new libro({
+      name: request.payload.name, 
+      genero: request.payload.genero,
+      autor: request.payload.autor,
+      añoPublicacion: request.payload.añoPublicacion,
+      editorial: request.payload.editorial,
+      descripcion: request.payload.descripcion,
+      keyword: request.payload.keyword,
+      total: request.payload.total,
+      disponibles: request.payload.disponibles
     });
-    newStudent.save();
-    console.log('student saved');
+    newLibro.save();
+    console.log('libro saved');
     return reply('ok');
   }
 }
+exports.updateLibro = {
+  handler: function(request, reply){
+    User.findOne({'Id': request.params.Id}, function(err, libro){
+      if (!err) {
+        libro.name = request.payload.name, 
+        libro.genero = request.payload.genero,
+        libro.autor = request.payload.autor,
+        libro.añoPublicacion = request.payload.añoPublicacion,
+        libro.editorial = request.payload.editorial,
+        libro.descripcion = request.payload.descripcion,
+        libro.keyword = request.payload.keyword,
+        libro.total = request.payload.total,
+        libro.disponibles = request.payload.disponibles
+        libro.save(libro);
+      }
+    });
+  }
+}
+exports.deleteLibro = {
+  handler: function (request, reply) {
+    User.findOne({ 'Id': request.params.Id }, function (err, libro) {
+      if (!err && user) {
+        libro.remove();
+        return reply({ message: "Operation Successful"});
+      }
+    });
+  }
+}
+
+
